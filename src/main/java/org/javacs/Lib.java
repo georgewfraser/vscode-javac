3e5ac39a4a1ecc7f210d43f46f435214d9e70510
Leigh McCulloch <leigh@mcchouse.com>
2019-02-18 06:45:06 +0000

    Make javaLsFlag.txt and lib/src.zip optional
    What
    ===
    Make javaLsFlag.txt and lib/src.zip optional.
    
    Why
    ===
    When using this tool standalone the lib/src.zip file doesn't seem to be
    packaged inside what is jlinked and exceptions are raised because it
    cannot be found. From the tests I've run it doesn't seem to make any
    difference if the zip file is present or not, at least to the features I
    am using. I don't really understand what the txt and zip file are for,
    but it seems like this language server
    protocol can be used without them.

--- src/main/java/org/javacs/Lib.java
+++ src/main/java/org/javacs/Lib.java
@@ -1,15 +1,15 @@
 package org.javacs;
 
+import java.util.Optional;
 import java.nio.file.*;
 
 class Lib {
-    static Path installRoot() {
+    static Optional<Path> installRoot() {
         var root = Paths.get(".").toAbsolutePath();
         var p = root;
         while (p != null && !Files.exists(p.resolve("javaLsFlag.txt"))) p = p.getParent();
-        if (p == null) throw new RuntimeException("Couldn't find javaLsFlag.txt in any parent of " + root);
-        return p;
+        return Optional.ofNullable(p);
     }
 
-    static final Path SRC_ZIP = installRoot().resolve("lib/src.zip");
+    static final Optional<Path> SRC_ZIP = installRoot().map(path -> path.resolve("lib/src.zip"));
 }
