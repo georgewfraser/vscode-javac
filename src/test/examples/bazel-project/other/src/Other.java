3c8f269e7d8e4347927be9c349f11225c37c7e06
Henry Kupty <hkupty@gmail.com>
2020-07-29 23:11:33 +0200

    Test multiple subprojects

---
+++ src/test/examples/bazel-project/other/src/Other.java
@@ -0,0 +1,6 @@
+class Other {
+    public static void main(String[] args) {
+        System.out.println("I'm the other class");
+    }
+}
+
--- src/test/java/org/javacs/config/InferBazelConfigTest.java
+++ src/test/java/org/javacs/config/InferBazelConfigTest.java
@@ -9,25 +9,33 @@ import org.junit.Test;
 public class InferBazelConfigTest {
     @Test
     public void bazelClassPath() {
-        var bazel = BazelConfig.buildConfig(Paths.get("src/test/examples/bazel-project"));
-        assertThat(bazel.classpath(), contains(hasToString(endsWith("guava-18.0.jar"))));
+        var bazel = Config.buildBazelConfig(Paths.get("src/test/examples/bazel-project"), "bazel");
+        var classpath = bazel.classpath();
+        System.out.println(classpath);
+        assertThat(classpath, hasItem(hasToString(endsWith("guava-18.0.jar"))));
     }
 
     @Test
     public void bazelClassPathInSubdir() {
-        var bazel = BazelConfig.buildConfig(Paths.get("src/test/examples/bazel-project/hello"));
-        assertThat(bazel.classpath(), contains(hasToString(endsWith("guava-18.0.jar"))));
+        var bazel = Config.buildBazelConfig(Paths.get("src/test/examples/bazel-project/hello"), "bazel");
+        var classpath = bazel.classpath();
+        System.out.println(classpath);
+        assertThat(classpath, contains(hasToString(endsWith("guava-18.0.jar"))));
     }
 
     @Test
     public void bazelDocPath() {
-        var bazel = BazelConfig.buildConfig(Paths.get("src/test/examples/bazel-project"));
-        assertThat(bazel.sourcepath(), contains(hasToString(endsWith("guava-18.0-sources.jar"))));
+        var bazel = Config.buildBazelConfig(Paths.get("src/test/examples/bazel-project"), "bazel");
+        var sourcepath = bazel.sourcepath();
+        System.out.println(sourcepath);
+        assertThat(sourcepath, hasItem(hasToString(endsWith("guava-18.0-sources.jar"))));
     }
 
     @Test
     public void bazelDocPathInSubdir() {
-        var bazel = BazelConfig.buildConfig(Paths.get("src/test/examples/bazel-project/hello"));
-        assertThat(bazel.sourcepath(), contains(hasToString(endsWith("guava-18.0-sources.jar"))));
+        var bazel = Config.buildBazelConfig(Paths.get("src/test/examples/bazel-project/hello"), "bazel");
+        var sourcepath = bazel.sourcepath();
+        System.out.println(sourcepath);
+        assertThat(sourcepath, contains(hasToString(endsWith("guava-18.0-sources.jar"))));
     }
 }
