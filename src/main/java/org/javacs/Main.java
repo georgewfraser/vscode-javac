8004869c0e5211fc186d2697d8ef4f4a306536b9
Henry Kupty <hkupty@gmail.com>
2020-07-29 13:46:19 +0200

    Add CLI parsing
    This also adds configuration for logging to file as well as `--help` flag.

--- src/main/java/org/javacs/Main.java
+++ src/main/java/org/javacs/Main.java
@@ -3,6 +3,7 @@ package org.javacs;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.FileHandler;
 import org.javacs.lsp.*;
 
 public class Main {
@@ -16,22 +17,67 @@ public class Main {
         }
     }
 
+    private static class CliArgs {
+
+      public boolean quiet;
+      public boolean help;
+      public boolean file;
+
+      private CliArgs(){
+        quiet = false;
+        help = false;
+        file = false;
+      }
+
+      public static String help(){
+        return "java-language-server [options]\n"
+             + "\n"
+             + "  --quiet             Suppress output\n"
+             + "  --file              Logs to file\n"
+             + "  --help              Shows this help message\n";
+      }
+
+      private static CliArgs parseCliArgs(String[] args){
+        CliArgs parsed = new CliArgs();
+        Arrays.stream(args).forEach(arg -> {
+            if ("--quiet".equals(arg)) {
+              parsed.quiet = true;
+            } else if ("--file".equals(arg)) {
+              parsed.file = true;
+            } else if ("--help".equals(arg)) {
+              parsed.help = true;
+            } else {
+              System.out.println("Unexpected argument '%s'".format(arg));
+              System.out.println(help());
+              System.exit(1);
+            }
+        });
+
+        return parsed;
+      }
+    }
+
     public static void main(String[] args) {
-        boolean quiet = Arrays.stream(args).anyMatch("--quiet"::equals);
+      CliArgs parsed = CliArgs.parseCliArgs(args);
 
-        if (quiet) {
-            LOG.setLevel(Level.OFF);
+      if (parsed.help){
+        System.out.println(CliArgs.help());
+        System.exit(0);
+      }
+
+      try {
+        if (parsed.file) {
+          Logger.getLogger("").addHandler(new FileHandler("javacs.%u.log", false));
+        } else if (parsed.quiet) {
+          LOG.setLevel(Level.OFF);
         }
+        setRootFormat();
 
-        try {
-            // Logger.getLogger("").addHandler(new FileHandler("javacs.%u.log", false));
-            setRootFormat();
+        LSP.connect(JavaLanguageServer::new, System.in, System.out);
+      } catch (Throwable t) {
+        LOG.log(Level.SEVERE, t.getMessage(), t);
 
-            LSP.connect(JavaLanguageServer::new, System.in, System.out);
-        } catch (Throwable t) {
-            LOG.log(Level.SEVERE, t.getMessage(), t);
-
-            System.exit(1);
-        }
+        System.exit(1);
+      }
     }
 }
