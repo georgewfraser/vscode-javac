f546f07e3cd94bf4f0ec3bc0f095379142705d82
ece_tavasli <ece.tavasli@codility.com>
2020-12-22 16:12:16 +0100

    Remove null elements from 'unused' before iterating
    Since LS performs async calls to 'lint' method, there is
    a chance that the items added to 'unused' HashMap can end
    up being 'null', therefore the next call which calls
    '.toString()' on 'unused's elements, can cause LS to crash.
    (experienced it several times, but unfortunately couldn't
    figure out the context it happens and reproduce it yet -
    therefore no automated test yet. :-().
    
    In order to prevent such failures, this commit is removing 'null'
    elements of 'unused' before the next call, which calls '.toString()'
    on its elements.

--- src/main/java/org/javacs/markup/WarnUnused.java
+++ src/main/java/org/javacs/markup/WarnUnused.java
@@ -5,14 +5,12 @@ import com.sun.source.util.JavacTask;
 import com.sun.source.util.TreePath;
 import com.sun.source.util.TreeScanner;
 import com.sun.source.util.Trees;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+
 import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
+import java.util.*;
 
 class WarnUnused extends TreeScanner<Void, Void> {
     // Copied from TreePathScanner
@@ -55,6 +53,9 @@ class WarnUnused extends TreeScanner<Void, Void> {
         unused.addAll(privateDeclarations.keySet());
         unused.addAll(localVariables.keySet());
         unused.removeAll(used);
+        // Remove if there are any null elements somehow ended up being added
+        // during async work which calls `lint`
+        unused.removeIf(Objects::isNull);
         // Remove if <error > field was injected while forming the AST
         unused.removeIf(i -> i.toString().equals("<error>"));
         return unused;
